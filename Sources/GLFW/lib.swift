67b0a00f0a74c395e67c53527de05e67f2456107
Naithar <devias.naith@gmail.com>
2017-01-16 15:12:27 +0300

    window hints

--- Sources/GLFW/lib.swift
+++ Sources/GLFW/lib.swift
@@ -1,5 +1,9 @@
 import CGLFW
 
+extension Int {
+    
+    static let dontCare = Int(GLFW_DONT_CARE)
+}
 public enum glfw {
     
     public struct Rect {
@@ -66,11 +70,205 @@ public enum glfw {
 //        glfwSwapInterval(value)
 //    }
     
-    public func set(swapInterval value: Int) {
+    public static func set(swapInterval value: Int) {
         glfwSwapInterval(Int32(value))
     }
     
+    public static func set(windowHint hint: Window.Hint) {
+        glfwWindowHint(hint.name, hint.value)
+    }
+    
     public final class Window {
+        
+        public enum Hint {
+            
+            internal var name: Int32 {
+                switch self {
+                case .resizable:
+                    return GLFW_RESIZABLE
+                case .visible:
+                    return GLFW_VISIBLE
+                case .decorated:
+                    return GLFW_DECORATED
+                case .focused:
+                    return GLFW_FOCUSED
+                case .iconify:
+                    return GLFW_AUTO_ICONIFY
+                case .floating:
+                    return GLFW_FLOATING
+                case .redBits:
+                    return GLFW_RED_BITS
+                case .greenBits:
+                    return GLFW_GREEN_BITS
+                case .blueBits:
+                    return GLFW_BLUE_BITS
+                case .alphaBits:
+                    return GLFW_ALPHA_BITS
+                case .depthBits:
+                    return GLFW_DEPTH_BITS
+                case .stencilBits:
+                    return GLFW_STENCIL_BITS
+                case .accumRedBits:
+                    return GLFW_ACCUM_RED_BITS
+                case .accumGreenBits:
+                    return GLFW_ACCUM_GREEN_BITS
+                case .accumBlueBits:
+                    return GLFW_ACCUM_BLUE_BITS
+                case .accumAlphaBits:
+                    return GLFW_ACCUM_ALPHA_BITS
+                case .auxBuffers:
+                    return GLFW_AUX_BUFFERS
+                case .samples:
+                    return GLFW_SAMPLES
+                case .refreshRate:
+                    return GLFW_REFRESH_RATE
+                case .stereo:
+                    return GLFW_STEREO
+                case .sRGBCapable:
+                    return GLFW_SRGB_CAPABLE
+                case .doubleBuffer:
+                    return GLFW_DOUBLEBUFFER
+                case .clientAPI:
+                    return GLFW_CLIENT_API
+                case .versionMajor:
+                    return GLFW_VERSION_MAJOR
+                case .versionMinor:
+                    return GLFW_VERSION_MINOR
+                case .contextRobustness:
+                    return GLFW_CONTEXT_ROBUSTNESS
+                case .contextReleaseBehavior:
+                    return GLFW_CONTEXT_RELEASE_BEHAVIOR
+                case .openGLForwardCompat:
+                    return GLFW_OPENGL_FORWARD_COMPAT
+                case .openGLDebugContext:
+                    return GLFW_OPENGL_DEBUG_CONTEXT
+                case .openGLProfile:
+                    return GLFW_OPENGL_PROFILE
+                }
+            }
+            
+            internal var value: Int32 {
+                switch self {
+                    case .resizable(let value),
+                         .visible(let value),
+                         .decorated(let value),
+                         .focused(let value),
+                         .iconify(let value),
+                         .floating(let value),
+                         .stereo(let value),
+                         .sRGBCapable(let value),
+                         .doubleBuffer(let value),
+                         .openGLForwardCompat(let value),
+                         .openGLDebugContext(let value),
+                         .openGLProfile(let value):
+                        return value ? GLFW_TRUE : GLFW_FALSE
+                case .redBits(let value),
+                     .greenBits(let value),
+                     .blueBits(let value),
+                     .alphaBits(let value),
+                     .depthBits(let value),
+                     .stencilBits(let value),
+                     .accumRedBits(let value),
+                     .accumGreenBits(let value),
+                     .accumBlueBits(let value),
+                     .accumAlphaBits(let value),
+                     .auxBuffers(let value),
+                     .samples(let value),
+                     .refreshRate(let value),
+                     .versionMajor(let value),
+                     .versionMinor(let value):
+                    return Int32(value)
+                case .clientAPI(let value):
+                    return value.raw
+                case .contextRobustness(let value):
+                    return value.raw
+                case .contextReleaseBehavior(let value):
+                    return value.raw
+                }
+            }
+            
+            case resizable(Bool)
+            case visible(Bool)
+            case decorated(Bool)
+            case focused(Bool)
+            case iconify(Bool)
+            case floating(Bool)
+            case redBits(Int)
+            case greenBits(Int)
+            case blueBits(Int)
+            case alphaBits(Int)
+            case depthBits(Int)
+            case stencilBits(Int)
+            case accumRedBits(Int)
+            case accumGreenBits(Int)
+            case accumBlueBits(Int)
+            case accumAlphaBits(Int)
+            case auxBuffers(Int)
+            case samples(Int)
+            case refreshRate(Int)
+            case stereo(Bool)
+            case sRGBCapable(Bool)
+            case doubleBuffer(Bool)
+            
+            public enum ClientAPI {
+                case openGL
+                case openGLES
+                
+                internal var raw: Int32 {
+                    switch self {
+                    case .openGL:
+                        return GLFW_OPENGL_API
+                    case .openGLES:
+                        return GLFW_OPENGL_ES_API
+                    }
+                }
+            }
+            
+            case clientAPI(ClientAPI)
+            case versionMajor(Int)
+            case versionMinor(Int)
+            
+            public enum ContextRobustness {
+                case none
+                case noResetNotification
+                case loseContextOnReset
+                
+                internal var raw: Int32 {
+                    switch self {
+                    case .none:
+                        return GLFW_NO_ROBUSTNESS
+                    case .noResetNotification:
+                        return GLFW_NO_RESET_NOTIFICATION
+                    case .loseContextOnReset:
+                        return GLFW_LOSE_CONTEXT_ON_RESET
+                    }
+                }
+            }
+            
+            case contextRobustness(ContextRobustness)
+            
+            public enum ContextReleaseBehavior {
+                case any
+                case flush
+                case none
+                
+                internal var raw: Int32 {
+                    switch self {
+                    case .any:
+                        return GLFW_ANY_RELEASE_BEHAVIOR
+                    case .flush:
+                        return GLFW_RELEASE_BEHAVIOR_FLUSH
+                    case .none:
+                        return GLFW_RELEASE_BEHAVIOR_NONE
+                    }
+                }
+            }
+            
+            case contextReleaseBehavior(ContextReleaseBehavior)
+            case openGLForwardCompat(Bool)
+            case openGLDebugContext(Bool)
+            case openGLProfile(Bool)
+        }
         
         public struct Framebuffer {
             
@@ -120,7 +318,10 @@ public enum glfw {
             return glfwWindowShouldClose(pointer) == 1
         }
         
-        public init?(width: Int, height: Int, title: String = "", monitor: Monitor? = nil) {
+        public init?(width: Int, height: Int, title: String = "", monitor: Monitor? = nil, hints: [Hint] = []) {
+            for hint in hints {
+                glfw.set(windowHint: hint)
+            }
             self.pointer = glfwCreateWindow(Int32(width),
                                             Int32(height),
                                             title,
